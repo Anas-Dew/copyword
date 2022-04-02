fb7266a99836e423c465258aecb43edaa94f5fae
Anas Raza <mailtoanas@duck.com>
2022-04-02 11:16:19 +0530

    Update gui.py

--- bin/gui.py
+++ bin/gui.py
@@ -17,8 +17,8 @@ existed_account_schema = ""
 
 root = Tk()
 
-# root.geometry("350x200")
-root.geometry("350x500")
+root.geometry("350x260")
+# root.geometry("350x500")
 root.title("CopyWord")
 root.configure(background='#1e1e21')
 root.resizable('False','False')
@@ -30,7 +30,7 @@ status_bar.pack(side="bottom",fill="x")
 
 
 # --------------------------------------------------------------------------------------
-def connection_status():
+def read_existing_login_from_local():
     status_bar['text'] = "Loading..."
 
     global existed_account_schema
@@ -51,8 +51,8 @@ def connection_status():
         status_bar['text'] = "Login Succees"
         status_bar['bg'] = "#03700c"
         
+        keeping_the_server_updated()
         login_or_signup_screen("LOGOUT")
-        # keeping_the_server_updated()
 
     except:
         status_bar['text'] = "You're logged out !!!"
@@ -86,30 +86,44 @@ password.insert(0, 'Password')
 
 def user_login():
     global existed_account_schema
+    try:
 
-    existed_account_schema = {
-        "email" : email.get(),
-        "name" : {userbase.find_one({'email' : f'{email.get()}'})['name']},
-        "password" : password.get()
-    }
+        existed_account_schema = {
+            "email" : email.get(),
+            "name" : {userbase.find_one({'email' : f'{email.get()}'})['name']},
+            "password" : password.get()
+        }
 
-    save_logs_on_system(existed_account_schema)
-    keeping_the_server_updated()
+        save_logs_on_system(existed_account_schema)
+        keeping_the_server_updated()
+        login_or_signup_screen("LOGOUT")
+
+    except:
+
+        pass
+
 
 def create_my_account():
     global existed_account_schema
 
-    new_account_schema = {
-        "email" : email.get(),
-        "name" : user_name.get(),
-        "password" : password.get()
-    }
+    try:
+        new_account_schema = {
+            "email" : email.get(),
+            "name" : user_name.get(),
+            "password" : password.get()
+        }
 
-    userbase.insert_one(new_account_schema)
-    new_account_schema = existed_account_schema
-    save_logs_on_system(existed_account_schema)
-    keeping_the_server_updated()
+        existed_account_schema = new_account_schema
+        print(new_account_schema)
+        userbase.insert_one(new_account_schema)
+        save_logs_on_system(existed_account_schema)
+        keeping_the_server_updated()
+
+        login_or_signup_screen("LOGOUT")
     
+    except:
+        pass
+
 
 
 
@@ -121,6 +135,7 @@ def login_or_signup_screen(auth_method:str):
 
     if auth_method == "LOGIN" :
 
+        root.after_cancel(keeping_the_server_updated)
         status_bar['text'] = "You're logged out !!!"
         status_bar['bg'] = "#70030a"
 
@@ -136,8 +151,7 @@ def login_or_signup_screen(auth_method:str):
         
         login.pack(padx=2, pady=7)
         I_dont_have_account.pack()
-
-
+        
 
     elif auth_method == "SIGNUP" :
 
@@ -149,15 +163,29 @@ def login_or_signup_screen(auth_method:str):
         user_name.pack(pady=3)
         signup.pack(padx=2, pady=7)
         back_to_previous_menu.pack()
-    
+
     elif auth_method == "LOGOUT" :
-        
+
+        email.pack_forget()
+        password.pack_forget()        
+
+        login.pack_forget()
+        I_dont_have_account.pack_forget()
+
+        user_name.pack_forget()
+        signup.pack_forget()
+        back_to_previous_menu.pack_forget()
+
         signed_as_user_name_shown_on_screen.pack(pady=10)
         logout_button.pack(padx=2, pady=7)
+
+        status_bar['text'] = "Login Succees"
+        status_bar['bg'] = "#03700c"
+
         
 
     else:
-        connection_status()
+        read_existing_login_from_local()
 
 
 def create_and_update_new_word_instance_on_server():
@@ -170,7 +198,7 @@ def reading_new_clipboard_from_server():
 
     new_clipboard_object = userbase.find_one({"email" : existed_account_schema["email"]})['clipboard']
     pc.copy(new_clipboard_object)
-    
+
 def keeping_the_server_updated():
     
         create_and_update_new_word_instance_on_server()
@@ -183,6 +211,6 @@ def keeping_the_server_updated():
 
 
 if __name__ == "__main__":
-    connection_status()
-    keeping_the_server_updated()
+    read_existing_login_from_local()
+    # keeping_the_server_updated()
     root.mainloop()
