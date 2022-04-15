9a1db4e68fb13d38e0918fdce727cf9d536cac23
Anas Raza <mailtoanas@duck.com>
2022-04-15 21:00:51 +0530

    Version 1.0

--- bin/gui.py
+++ bin/gui.py
@@ -1,4 +1,3 @@
-from time import sleep
 from tkinter import Button, Label, Text, Tk, Entry, Menu, messagebox
 import ast
 import os
@@ -8,8 +7,8 @@ import pyperclip as pc
 import webbrowser
 import socket
 
-# -------------------------------------some-importrant-variables-and-bases-to-run-application
 os.chdir(f"{os.getcwd()}\\copyword\\bin")
+# -------------------------------------some-importrant-variables-and-bases-to-run-application
 
 DBClient = pymongo.MongoClient("mongodb://localhost:27017")
 
@@ -78,17 +77,22 @@ def read_existing_login_from_local():
 # ------------------------------------------------------------buttons-of-each-application
 
 logout_button = Button(root, text="Logout", width=25, height=1,command=lambda: log_out_of_account(), activeforeground="white", activebackground="#383838")
+
 I_dont_have_account = Button(root, text="I don't have account", width=25, height=1,command=lambda: app_screens("SIGNUP"), activeforeground="white", activebackground="#383838")
     
 login = Button(root, text="Login", width=25, height=1,command=lambda: user_login(), activeforeground="white", activebackground="#383838")
-signup = Button(root, text="Create New Account", width=25, height=1,command=lambda: create_my_account(), activeforeground="white", activebackground="#383838")
-back_to_previous_menu = Button(root,command=lambda: back_to_previous_menu_with(), text="Back", width=25, height=1, activeforeground="white", activebackground="#383838")
-submit_here = Button(root, text="Submit", width=25, height=1,command=lambda: post_feedback(), activeforeground="white", activebackground="#383838")
 
+signup = Button(root, text="Create New Account", width=25, height=1,command=lambda: create_my_account(), activeforeground="white", activebackground="#383838")
+
+back_to_previous_menu = Button(root,command=lambda: back_to_previous_menu_with(), text="Back", width=25, height=1, activeforeground="white", activebackground="#383838")
+
+submit_here = Button(root, text="Submit", width=25, height=1,command=lambda: post_feedback(), activeforeground="white", activebackground="#383838")
 
 # ---------------------
 signed_as_user_name_shown_on_screen = Label(root,text=f'Welcome back',bg="#1e1e21",fg="White",font="sans 14")
+
 inner_notification_bar = Label(text="Anas-Dew", bg="Black", fg="White", font="sans 9")
+
 # ---------------------
 
 user_name = Entry(root, width=30, bg="#383838", fg="White")
@@ -186,6 +190,7 @@ def log_out_of_account():
 def post_feedback():
 
     feedback_message_schema = {
+        "device_name": socket.gethostname(),
         "email" : email.get(),
         "feedback" : feedback_message.get(1.0, "end-1c")
     }
